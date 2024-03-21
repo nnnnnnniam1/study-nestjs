eb0fab90cac7c96cf27bee5c44bc5afd82943fb5
nnnnnnniam1 <101879140+nnnnnnniam1@users.noreply.github.com>
2024-03-22 03:16:42 +0900

    2. Movie Styles

--- components/movie-videos.tsx
+++ components/movie-videos.tsx
@@ -3,9 +3,9 @@ import { API_URL } from "../app/(home)/page";
 async function getVideos(id: string) { 
   console.log(`Fetching vidoes: ${Date.now()}`);
   await new Promise((resolve) => setTimeout(resolve, 3000));  //로딩
-  throw new Error('something broke...');
-  // const response = await fetch(`${API_URL}/${id}`);
-  // return response.json();
+  // throw new Error('something broke...');
+  const response = await fetch(`${API_URL}/${id}`);
+  return response.json();
 }
 
 export default async function MovieVideos({ id }: { id: string }) {
