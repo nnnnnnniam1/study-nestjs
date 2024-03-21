0843c8ab52833bea1129d304c9b3318b5dfe91f7
nnnnnnniam1 <101879140+nnnnnnniam1@users.noreply.github.com>
2024-03-22 03:34:43 +0900

    3. Movie Trailers

--- components/movie-info.tsx
+++ components/movie-info.tsx
@@ -1,6 +1,6 @@
 import exp from "constants";
 import { API_URL } from "../app/(home)/page";
-
+import styles from '../styles/movie-info.module.css'
 async function getMovies(id:string) {
   await new Promise((resolve) => setTimeout(resolve, 5000));  //로딩
   const response = await fetch(`${API_URL}/${id}`);
@@ -10,7 +10,17 @@ async function getMovies(id:string) {
 
 export default async function MovieInfo({ id }: { id: string }) {
   const movie = await getMovies(id);
-  return <h6>{JSON.stringify(movie)}</h6>
+  return (
+    <div className={styles.container}>
+      <img className={styles.poster} src={movie.poster_path} alt={movie.title} />
+      <div className={styles.info} >
+        <h1 className={styles.title} >{movie.title}</h1>
+        <h3> * {movie.vote_average.toFixed(1)}</h3>
+        <p>{movie.overview}</p>
+        <a href={movie.hompage} target={"_blank"}>Homepage &rarr;</a>
+      </div>
+    </div>
+  );
   
 }
 
