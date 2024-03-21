3bef8d8a865a9ffae0a3b4f15eef196b2562ebaa
nnnnnnniam1 <101879140+nnnnnnniam1@users.noreply.github.com>
2024-03-22 01:39:27 +0900

    2. Server Side

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9a7f18e59d7bfacc"; }
+/******/ 		__webpack_require__.h = function() { return "44e7f4e825750ab7"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,23 +1,20 @@
-"use client"
+export const metadata = {
+  title: "Home",
+};
 
-import { useEffect, useState } from "react";
+const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
 
+async function getMovies() {
+  // const response = await fetch(URL).then(response => response.json());
+  await new Promise((resolve) => setTimeout(resolve, 5000));  //로딩
+  const response = await fetch(URL);
+  const json = await response.json();
+  return json;
+}
 
-export default function Page() {
-  const [isLoading, setIsLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-  const getMovies = async () => {
-    const response = await fetch("https://nomad-movies.nomadcoders.workers.dev/movies");
-    const json = await response.json();
-    setMovies(json);
-    setIsLoading(false);
-  }
-  useEffect(() => {
-    getMovies();
-  }, []);
+export default async function HomePage() {
+  const movies = await getMovies();
   return (
-    <div>
-      {isLoading ? "Loading..." : JSON.stringify(movies)}
-    </div>
-  );
+    <div>{JSON.stringify(movies)}</div>
+    );
 }
